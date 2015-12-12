f2011e76ff85a1bebce91ee093f1178c7f80af43
Dmitry Novik <d1mas1k4@yandex.ru>
2015-12-12 21:42:51 +0300

    Task is done.

---
+++ app/src/main/java/ru/ifmo/android_2015/homework5/DownloadService.java
@@ -0,0 +1,65 @@
+package ru.ifmo.android_2015.homework5;
+
+import android.app.IntentService;
+import android.content.Context;
+import android.content.Intent;
+import android.support.v4.content.LocalBroadcastManager;
+import android.util.Log;
+
+import java.io.File;
+import java.io.IOException;
+
+/**
+ * Created by novik on 11.12.15.
+ */
+public class DownloadService extends IntentService implements ProgressCallback {
+    private static final String TAG = DownloadService.class.getSimpleName();
+
+    public static final String FILE_TAG = "FILE_URL";
+    public static final String STATUS_TAG = "STATUS";
+    public static final String PROGRESS_TAG = "PROGRESS";
+    public static final String ACTION = "ACTION.DOWNLOAD_FILE";
+
+    public static final int DOWNLOADING = 0;
+    public static final int DONE = 1;
+    public static final int ERROR = -1;
+
+    @Override
+    public void onProgressChanged(int progress) {
+        Intent intent = new Intent(ACTION);
+
+        intent.putExtra(PROGRESS_TAG, progress);
+        intent.putExtra(STATUS_TAG, DOWNLOADING);
+
+        sendBroadcast(intent);
+    }
+
+    public DownloadService() {
+        super(TAG);
+
+        Log.d(TAG, "onCreate");
+    }
+
+    public void sendBroadcast(Intent intent) {
+        LocalBroadcastManager.getInstance(getApplicationContext()).sendBroadcast(intent);
+    }
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+        Log.d(TAG, "onHandleIntent");
+        String fileUrl = intent.getStringExtra(FILE_TAG);
+
+        try {
+            File destFile = FileUtils.createTempExternalFile(getApplicationContext(), "gz");
+            DownloadUtils.downloadFile(fileUrl, destFile, this);
+
+            sendBroadcast(new Intent(ACTION).putExtra(PROGRESS_TAG, 100).putExtra(STATUS_TAG, DONE));
+        } catch (IOException e) {
+            Log.e(TAG, "Downloading error occurred!");
+
+            sendBroadcast(new Intent(ACTION).putExtra(PROGRESS_TAG, -1).putExtra(STATUS_TAG, ERROR));
+
+            e.printStackTrace();
+        }
+    }
+}
