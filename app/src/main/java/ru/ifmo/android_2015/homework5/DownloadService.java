ce8b1c73c387f65b3daf3790fc8669ae1e90320c
Alexey Zabelkin <alex39-09@mail.ru>
2015-12-29 02:15:42 +0300

    no message

---
+++ app/src/main/java/ru/ifmo/android_2015/homework5/DownloadService.java
@@ -0,0 +1,36 @@
+package ru.ifmo.android_2015.homework5;
+
+import android.app.IntentService;
+import android.content.Intent;
+
+/**
+ * Created by Богдан on 27.12.2015.
+ */
+public class DownloadService extends IntentService {
+
+    public DownloadService() {
+        super("DownloadService");
+    }
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+        try {
+            InitSplashActivity.downloadFile(this, new ProgressCallback() {
+                @Override
+                public void onProgressChanged(int progress) {
+                    publishProgress(InitSplashActivity.DownloadState.DOWNLOADING, progress);
+                }
+            });
+            publishProgress(InitSplashActivity.DownloadState.DONE, 100);
+        } catch (Exception e) {
+            publishProgress(InitSplashActivity.DownloadState.ERROR, 100);
+        }
+    }
+
+    private void publishProgress(InitSplashActivity.DownloadState downloadState, int progress) {
+        Intent intent = new Intent("service");
+        intent.putExtra("state", downloadState);
+        intent.putExtra("progress", progress);
+        sendBroadcast(intent);
+    }
+}
